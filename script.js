7a8c90777944d8d19982249aae03800c01a42091
tasnimchy96 <tasnim.achy96@gmail.com>
2023-03-17 15:27:23 +0600

    Validate contact form

--- script.js
+++ script.js
@@ -8,3 +8,19 @@ function closeNav() {
 
 document.querySelector('.open').addEventListener('click', openNav);
 document.querySelector('.close').addEventListener('click', closeNav);
+
+// Validate contact form
+
+const form = document.getElementById('myForm');
+const emailField = document.getElementById('useremail');
+
+form.addEventListener('submit', (event) => {
+  if (emailField.value !== emailField.value.toLowerCase()) {
+    event.preventDefault();
+    const error = document.createElement('div');
+    error.innerText = 'Email must be in lowercase';
+    error.style.color = 'red';
+    const submitButton = document.getElementById('submit');
+    submitButton.parentNode.insertBefore(error, submitButton.nextSibling);
+  }
+});
